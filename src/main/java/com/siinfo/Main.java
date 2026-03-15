bff69f0f1beb3cdba9ae9c2af4dca39cb38c8474
Helvy Tiana Rossa <20240910034@uniku.ac.id>
2026-03-15 16:49:49 +0700

    Update student ID in Main.java

--- src/main/java/com/siinfo/Main.java
+++ src/main/java/com/siinfo/Main.java
@@ -10,7 +10,7 @@ public class Main {
      */
     public static void main(String[] args) {
         System.out.println("Nama: Helvy Tiana Rossa");
-        System.out.println("NIM: isi NIM kamu");
+        System.out.println("NIM: 20240910034");
         System.out.println("Program Studi: Sistem Informasi");
         System.out.println("Motivasi: Saya belajar Java supaya bisa membuat program sendiri dan mengembangkan kemampuan saya di bidang teknologi.");
     }
