c9583640aaec62328894ea5b9a9d05b0da91f2ee
Helvy Tiana Rossa <20240910034@uniku.ac.id>
2026-03-15 16:45:58 +0700

    first commit

---
+++ src/main/java/com/siinfo/Main.java
@@ -0,0 +1,17 @@
+package com.siinfo;
+
+/**
+ * Program Identitas Mahasiswa Sistem Informasi
+ */
+public class Main {
+
+    /**
+     * Method utama program
+     */
+    public static void main(String[] args) {
+        System.out.println("Nama: Helvy Tiana Rossa");
+        System.out.println("NIM: isi NIM kamu");
+        System.out.println("Program Studi: Sistem Informasi");
+        System.out.println("Motivasi: Saya belajar Java supaya bisa membuat program sendiri dan mengembangkan kemampuan saya di bidang teknologi.");
+    }
+}
